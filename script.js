9eaca06b3658c0dc4b58db28d6878eba1a39a72c
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-11-18 13:19:30 +0200

    styles the background and text and added the create board function to the JS

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(32);
+
+console.log("hey");
+});
+
+function createBoard(size){
+    let board = document.querySelector(".container");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board.insertAdjacentElement("beforeend", div);
+        
+    }
+}
